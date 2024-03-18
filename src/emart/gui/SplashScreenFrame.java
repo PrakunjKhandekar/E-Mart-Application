3420b6ec1c9a96c365ea51290425105c65829e9b
PrakunjKhandekar <prakunjkhandekar@gmail.com>
2024-03-18 16:11:29 +0530

    commit

--- src/emart/gui/SplashScreenFrame.java
+++ src/emart/gui/SplashScreenFrame.java
@@ -5,6 +5,9 @@
  */
 package emart.gui;
 
+import java.util.Random;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author PRAKUNJ
@@ -16,6 +19,10 @@ public class SplashScreenFrame extends javax.swing.JFrame {
      */
     public SplashScreenFrame() {
         initComponents();
+        this.setLocationRelativeTo(null);
+        jProgressBar.setStringPainted(true);
+        SplashThread sp = new SplashThread();
+        sp.start();
     }
 
     /**
@@ -27,17 +34,41 @@ public class SplashScreenFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jProgressBar = new javax.swing.JProgressBar();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/emart/images/SplashScreen.jpg"))); // NOI18N
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jProgressBar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 449, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jProgressBar, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -79,5 +110,30 @@ public class SplashScreenFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JProgressBar jProgressBar;
     // End of variables declaration//GEN-END:variables
+
+    class SplashThread extends Thread {
+
+        public void run() {
+            int count = 1;
+            Random rd = new Random();
+            while(jProgressBar.getValue()<jProgressBar.getMaximum()){
+                try{
+                    jProgressBar.setValue(count);
+                    Thread.sleep(1200);
+                    count=count+rd.nextInt(100);
+                }
+                catch(InterruptedException ex){
+                    JOptionPane.showMessageDialog(null,"Thread interrupted");
+                    ex.printStackTrace();
+                }
+            }
+            dispose();
+            LoginFrame log = new LoginFrame();
+            log.setVisible(true);
+        }
+    }
 }
